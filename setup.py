c9989008c6441b025592083b8de9a68e6d7bdc2f
Andrew Walker <walker.ab@gmail.com>
2013-11-03 20:02:46 +1100

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name         = 'pydubins',
+      version      = '1.0',
+      description  = "Dubin's curves for kinematic planning",
+      author       = 'Andrew Walker',
+      author_email = 'walker.ab@gmail.com',
+      packages     = ['pydubins']
+)
