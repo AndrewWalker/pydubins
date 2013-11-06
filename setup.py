f8c92dd4f6e855b935f70aa9f63fa1ba0c31c69a
Andrew Walker <walker.ab@gmail.com>
2013-11-07 09:38:49 +1100

    adding classifiers and filling in setup details prior to pypi release

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
+try:
+    from Cython.Build import cythonize
+except:
+    print 'Could not find cython, install it first'
+    raise
 
 extensions = [
     Extension("pydubins", ["pydubins/src/dubins.c", "pydubins/pydubins.pyx"],
@@ -9,6 +13,20 @@ extensions = [
 ]
 
 setup(
-    ext_modules = cythonize(extensions)
+    author       = "Andrew Walker",
+    author_email = "walker.ab@gmail.com",
+    url          = "http://github.com/AndrewWalker/pydubins",
+    classifiers  = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        # TODO - further testing required to show which platforms
+        #        will work correctly, but any changes should be
+        #        fairly minor
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+    ext_modules  = cythonize(extensions),
 )
 
