b8cd2c9e0e95b026c91f614751f4c9caf75043ec
Andrew Walker <walker.ab@gmail.com>
2013-11-07 17:26:20 +1100

    docstrings

--- pydubins/pydubins.pyx
+++ pydubins/pydubins.pyx
@@ -27,10 +27,29 @@ cdef extern from "dubins.h":
     int dubins_path_sample_many( DubinsPath* path, DubinsPathSamplingCallback cb, double stepSize, void* user_data )
 
 cdef int callback(double q[3], double t, void* f):
+    """This is the c- proxy callback that delegates back to Python
+    """
     qn = (q[0], q[1], q[2])
     return (<object>f)(qn, t)
 
 def sample_dubins_path( q0, q1, rho, step_size ):
+    """Discrete sampling of a Dubin's path
+
+    Parameters
+    ----------
+    q0 : array-like shape = (3,), dtype = float
+        initial configuration (x0, y0, theta0)
+    q1 : array-like shape = (3,), dtype = float
+        final configuration (x1, y1, theta1)
+    rho : float
+        equivalent to turning radius (forward velocity / angular rate)
+    step_size : float
+        sampling interval along the path
+
+    Notes
+    -----
+    theta = 0 is along the line x = 0
+    """
     cdef double _q0[3]
     cdef double _q1[3]
     for i in [0, 1, 2]:
