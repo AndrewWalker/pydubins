0dd27155f36d5394902aa01b1ef7fda0f3d85506
Andrew Walker <walker.ab@gmail.com>
2014-03-23 20:15:16 +1100

    add 3.3 to the trove classifiers

--- setup.py
+++ setup.py
@@ -50,6 +50,8 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
         'Operating System :: POSIX :: Linux',
         'Topic :: Scientific/Engineering :: Mathematics',
     ],
